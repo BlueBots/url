f881fc0ecb1beb4e0fa9d04c3aedbb8317b5b440
BlueBoy <66094947+BlueBots@users.noreply.github.com>
2023-07-07 02:01:16 -0400

    Update pastebin_proxy.php

--- pastebin_proxy.php
+++ pastebin_proxy.php
@@ -1,20 +1,20 @@
 <?php
-$url = 'https://pastebin.com/api/api_post.php';
-$params = [
-  'api_dev_key' => $_POST['EiCL7bT6Xe2T8xHGJky0HCRd7HYerYKx'],
-  'api_option' => 'paste',
-  'api_paste_code' => $_POST['api_paste_code'],
-  'api_paste_private' => '0'
-];
+// Retrieve the POST data from the client-side JavaScript code
+$requestData = file_get_contents('php://input');
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
+// Define the Pastebin API endpoint
+$pastebinAPI = 'https://pastebin.com/api/api_post.php';
+
+// Make a POST request to the Pastebin API
+$ch = curl_init($pastebinAPI);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $requestData);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
 $response = curl_exec($ch);
 curl_close($ch);
 
+// Set the appropriate response headers
 header('Content-Type: application/json');
 echo $response;
 ?>
